5b7f6d66e50636858f8471de1bc972d488207e9d
Hozt <josecmasters@gmail.com>
2025-03-25 18:30:05 -0600

    Agregando carpeta SistemGestionCalidad para avance de JWT

---
+++ SistemGestionCalidad/src/main/java/com/sgc/SistemGestionCalidad/SistemGestionCalidadApplication.java
@@ -0,0 +1,13 @@
+package com.sgc.SistemGestionCalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemGestionCalidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemGestionCalidadApplication.class, args);
+	}
+
+}
---
+++ SistemGestionCalidad/src/test/java/com/sgc/SistemGestionCalidad/SistemGestionCalidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgc.SistemGestionCalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemGestionCalidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
